98f37d350f905cdc5c2bd5f5546d8bfa9fd57e20
Haoyu Li <aide0740@gmail.com>
2021-02-27 17:04:17 -0500

    change parse_argument function

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
 import numpy as np
 
-from model import AE
+from model.pointnet import PointNet as AE
 from process_data import *
 
 def inference(pd,model,batch_size,args):
@@ -57,8 +57,7 @@ def inference(pd,model,batch_size,args):
     else:
         return latent_all
 
-if __name__ == "__main__":
-    # input parsing
+def parse_arguments():
     parser = argparse.ArgumentParser(description='PointNet')
     parser.add_argument('--batch-size', type=int, default=32, metavar='N',
                         help='input batch size for training (default: 128)')
@@ -70,10 +69,10 @@ if __name__ == "__main__":
                         help='enables CUDA training')
     parser.add_argument('--have-label', action='store_true', default=False,
                         help='have label for classification instead of autoencoder')
-    parser.add_argument('-p', '--phase', type=int,default=0,dest="phase",
-                        help='phase')
-    # parser.add_argument('--seed', type=int, default=1, metavar='S',
-    #                     help='random seed (default: 1)')
+    parser.add_argument('-p', '--patch-number', type=int,default=125,dest='p',
+                        help='how many patches in which one file is seperated')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=7, metavar='N',
                         help='how many batches to wait before logging training status')
     parser.add_argument('-w', '--load', dest='load', type=str,
@@ -89,8 +88,58 @@ if __name__ == "__main__":
     parser.add_argument('-k', dest='k', type=int, default=256, help='k in knn')
     parser.add_argument('-r', dest='r', type=float, default=0.2, help='r in ball query')
     parser.add_argument("--result-dir", dest="result_dir", type=str, default="states", help='the directory to save the result')
-    # parser.add_argument('--recon-length', dest='recon_length', type=int, default=256, help='r in ball query')
     args = parser.parse_args()
+    return args
+
+def train(epoch,args,loader):
+        model.train()
+
+        train_loss = 0
+        print("number of samples: ",len(pd))
+        for i, d in enumerate(loader):
+            if isinstance(d,list):
+                data = d[0][:,:,:args.dim].float().to(device)
+                if args.have_label:
+                    label = d[-1].to(device)
+                if args.mode=="ball" :
+                    mask = d[1].to(device)
+            else:
+                data = d[:,:,:args.dim].float().to(device)
+            optimizer.zero_grad()
+            if args.mode=="knn":
+                recon_batch = model(data) 
+            elif args.mode=="ball":
+                recon_batch = model(data,mask) 
+            if args.have_label:
+                loss_fn = nn.CrossEntropyLoss(reduction="mean")
+                loss = loss_fn(recon_batch,label)
+            else:
+                loss = model.loss(recon_batch, data)
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+
+            if i % args.log_interval == 0:
+                print('Train Epoch: {} [{:.0f}%]\tLoss: {:.6f}'.format(
+                    epoch, 
+                    100. * i / len(loader),
+                    loss.item(),
+                    ))
+            if i == len(loader)-1:
+                print('====> Epoch: {} Average loss: {:.4f}'.format(
+                    epoch, train_loss / len(loader)))
+        if not os.path.isdir(args.result_dir):
+            os.mkdir(args.result_dir)
+        save_dict = {
+            "state": model.state_dict(),
+            "config":args,
+        }
+        torch.save(save_dict,args.result_dir+'/CP{}.pth'.format(epoch))
+        print('Checkpoint {} saved !'.format(epoch))
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
     args.cuda = not args.no_cuda and torch.cuda.is_available()
     args.mode = "ball" if args.ball else 'knn'
     try:
@@ -98,13 +147,12 @@ if __name__ == "__main__":
     except KeyError:
         data_path = './data/'
 
-    # torch.manual_seed(args.seed)
+    torch.manual_seed(args.seed)
     device = torch.device("cuda" if args.cuda else "cpu")
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
     model = AE(args).float().to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer,0.85)
 
     if args.load:
         state_dict = torch.load(args.load)
@@ -114,75 +162,33 @@ if __name__ == "__main__":
         model.load_state_dict(state)
         print('Model loaded from {}'.format(args.load))
 
-    if args.phase == 0:
-        # train
-        print(args)
-        if args.source == "fpm":
-            file_list = collect_file(data_path+"/2016_scivis_fpm/0.44/run03/",args.source,shuffle=True)
-        elif args.source == "cos":
-            file_list = collect_file(data_path+"/ds14_scivis_0128/raw",args.source,shuffle=True)
-        for epoch in range(1, args.epochs + 1):
-            for i,f in enumerate(file_list):
-                print("file in process: ",f)
-                print("file processed {}/{}".format(i,len(file_list)))
-                if args.source == "fpm":
-                    data_source = vtk_reader(f)
-                elif args.source == "cos":
-                    data_source = sdf_reader(f)
-                if args.have_label:
-                    # read halo file if have label
-                    dir_name, real_file_name = os.path.split(f)
-                    timestep = int(real_file_name[-4:-2])
-                    if timestep == 0: 
-                        timestep = 100
-                    halo_file_name = dir_name+"/../rockstar/out_{}.list".format(timestep-2)
-                    halo_info = halo_reader(halo_file_name)
-                    pd = PointData(data_source,args,None,halo_info)
-                else:
-                    pd = PointData(data_source,args)
-                loader = DataLoader(pd, batch_size=args.batch_size, shuffle=True, drop_last=True)
-                model.train()
-                train_loss = 0
-                print("number of samples: ",len(pd))
-                for i, d in enumerate(loader):
-                    if isinstance(d,list):
-                        data = d[0][:,:,:args.dim].float().to(device)
-                        if args.have_label:
-                            label = d[-1].to(device)
-                        if args.mode=="ball" :
-                            mask = d[1].to(device)
-                    else:
-                        data = d[:,:,:args.dim].float().to(device)
-                    optimizer.zero_grad()
-                    if args.mode=="knn":
-                        recon_batch = model(data) 
-                    elif args.mode=="ball":
-                        recon_batch = model(data,mask) 
-                    if args.have_label:
-                        loss_fn = nn.CrossEntropyLoss(reduction="mean")
-                        loss = loss_fn(recon_batch,label)
-                    else:
-                        loss = model.loss(recon_batch, data)
-                    loss.backward()
-                    train_loss += loss.item()
-                    optimizer.step()
+    print(args)
 
-                    if i % args.log_interval == 0:
-                        print('Train Epoch: {} [{:.0f}%]\tLoss: {:.6f}'.format(
-                            epoch, 
-                            100. * i / len(loader),
-                            loss.item(),
-                            ))
-                    if i == len(loader)-1:
-                        print('====> Epoch: {} Average loss: {:.4f}'.format(
-                            epoch, train_loss / len(loader)))
-                if not os.path.isdir(args.result_dir):
-                    os.mkdir(args.result_dir)
-                save_dict = {
-                    "state": model.state_dict(),
-                    "config":args,
-                }
-                torch.save(save_dict,args.result_dir+'/CP{}.pth'.format(epoch))
-                print('Checkpoint {} saved !'.format(epoch))
-            scheduler.step()
+    # prepare data
+    if args.source == "fpm":
+        file_list = collect_file(data_path+"/2016_scivis_fpm/0.44/run03/",args.source,shuffle=True)
+    elif args.source == "cos":
+        file_list = collect_file(data_path+"/ds14_scivis_0128/raw",args.source,shuffle=True)
+    for epoch in range(1, args.epochs + 1):
+        for i,f in enumerate(file_list):
+            print("file in process: ",f)
+            print("file processed {}/{}".format(i,len(file_list)))
+            if args.source == "fpm":
+                data_source = vtk_reader(f)
+            elif args.source == "cos":
+                data_source = sdf_reader(f)
+            if args.have_label:
+                # read halo file if have label
+                dir_name, real_file_name = os.path.split(f)
+                timestep = int(real_file_name[-4:-2])
+                if timestep == 0: 
+                    timestep = 100
+                halo_file_name = dir_name+"/../rockstar/out_{}.list".format(timestep-2)
+                halo_info = halo_reader(halo_file_name)
+                pd = PointData(data_source,args,None,halo_info)
+            else:
+                pd = PointData(data_source,args)
+            loader = DataLoader(pd, batch_size=args.batch_size, shuffle=True, drop_last=True)
+            
+            train(epoch,args,loader)
 
